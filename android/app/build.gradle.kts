190e074b50dfae69bcaa386cc9eb485b8bbe2aa9
AWMA-apps <telecomandcoding@gmail.com>
2025-08-06 11:26:13 +0300

    remove firebase config from main

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.cicd_tutorial"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "29.0.13599879"
+    //ndkVersion = "29.0.13599879"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
